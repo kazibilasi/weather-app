09374dd1b7e5dff704aff4d4af5c777f8734f4af
kazibilsi <kazibilasi@gmail.com>
2024-03-09 09:23:48 +0600

    updated weather data

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const apiKey = '2f6d762279536158479e1e609f21420d'
-const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=dhaka'
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q='
 
-async function checkWeather(){
-    const response = await fetch(apiUrl + `&appid=${apiKey}`)
+const searchBox = document.querySelector('.search input')
+const searchBtn = document.querySelector('.search button')
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`)
     var data = await response.json()
     console.log(data)
 
     document.querySelector('.city').innerHTML = data.name;
-    document.querySelector('.temp').innerHTML = data.main.temp;
-    document.querySelector('.humidity').innerHTML = data.main.humidity;
-    document.querySelector('.wind').innerHTML = data.wind.speed;
+    document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + '°C';
+    document.querySelector('.humidity').innerHTML = data.main.humidity + ' %';
+    document.querySelector('.wind').innerHTML = data.wind.speed + ' km/h';
 }
 
-checkWeather()
+
+searchBtn.addEventListener('click',()=> {
+    checkWeather(searchBox.value)
+})
